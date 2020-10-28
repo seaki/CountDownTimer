8f241b9f7cbbc28274412b30aa6cd772eb227cfd
Se AKi <seaki@sastudio.jp>
2020-10-28 21:24:25 +0900

    Impl. CountDownTimer

--- app/src/main/java/jp/sastudio/countdowntimer/MainActivity.kt
+++ app/src/main/java/jp/sastudio/countdowntimer/MainActivity.kt
@@ -1,11 +1,32 @@
 package jp.sastudio.countdowntimer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    private var countDownTimer: CountDownTimer =
+        object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                var minute = millisUntilFinished / 1000 / 60
+                var second = millisUntilFinished / 1000 % 60
+                findViewById<TextView>(R.id.textViewCountDown).text =
+                    "%02d:%02d".format(minute, second)
+            }
+            override fun onFinish() {}
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.buttonStartPause).setOnClickListener {
+            countDownTimer.start()
+        }
+        findViewById<Button>(R.id.buttonReset).setOnClickListener {
+            countDownTimer.cancel()
+        }
     }
 }
